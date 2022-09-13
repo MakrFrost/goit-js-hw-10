323ce49ac54763bf9f31dc0f71cd354a52c85898
MakrFrost <frostmark@ukr.net>
2022-09-13 23:29:19 +0300

    Update+++

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './css/styles.css';
+import fetchCountries from './fetchCountries';
+const debounce = require('lodash.debounce');
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputSearch = document.querySelector('input#search-box');
+inputSearch.addEventListener(
+  'input',
+  debounce(event => {
+    event.preventDefault();
+    const searchingLetters = event.target.value;
+    fetchCountries(`${searchingLetters}`);
+  }, DEBOUNCE_DELAY)
+);
