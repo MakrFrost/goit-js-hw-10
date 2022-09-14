e16facb9cb668a0b160061607806703e6f4d0c18
MakrFrost <frostmark@ukr.net>
2022-09-14 15:26:12 +0300

    update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,12 +1,19 @@
-export default function fetchCountries(name) {
+function fetchCountries(name) {
   const url = 'https://restcountries.com/v3.1/name/';
 
   const options = '?fields=name,capital,population,flags,languages';
 
-  fetch(`${url}${name}${options}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  fetch(`${url}${name}${options}`)
+    .then(response => {
+      return response.json();
+    })
+    .then(massiveOfCountries => {
+      return [massiveOfCountries];
+    });
 }
+
+export default fetchCountries;
+
+// if (!response.ok) {
+//   throw new Error(response.status);
+// }
--- src/index.js
+++ src/index.js
@@ -7,11 +7,40 @@ import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 300;
 
 const inputSearch = document.querySelector('input#search-box');
+const countryListEl = document.querySelector('country-list');
+const countryInfoEl = document.querySelector('country-info');
+
 inputSearch.addEventListener(
   'input',
   debounce(event => {
     event.preventDefault();
     const searchingLetters = event.target.value;
-    fetchCountries(`${searchingLetters}`);
+    console.log(searchingLetters);
+    // fetchCountries(`${searchingLetters}`);
+    makeCountryList(fetchCountries(`${searchingLetters}`));
   }, DEBOUNCE_DELAY)
 );
+
+function makeCountryList(countries) {
+  const countryList = countries.map(country => {
+    return `<li class="country-list__item">
+  <img src="${country.flags.png}" width="100" />
+  <p class="country-list__name">${country.name.official}</p>
+</li>`;
+  });
+  for (const country of countryList) {
+    countryListEl.insertAdjacentHTML('afterbegin', country);
+  }
+}
+
+// function makeMarkupList(countries) {
+//   const countriesList = countries.map(country => {
+//     return `<li class="country-list__item">
+//             <img src=${country.flags.png} width="50" alt="flag">
+//             <span class="country-list__name">${country.name.official}</span>
+//         </li>`;
+//   });
+//   countriesList.forEach(markupCountry => {
+//     countriesListElem.insertAdjacentHTML('beforeend', markupCountry);
+//   });
+// }
