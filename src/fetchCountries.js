dd1202bc8e77692392b7ceac45fdc017efa588de
MakrFrost <frostmark@ukr.net>
2022-09-16 21:48:46 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,4 @@
 function fetchCountries(nameCountry) {
-  // const url = 'https://restcountries.com/v3.1/name/';
-  // const options = '?fields=name,capital,population,flags,languages';
-
   return fetch(
     `https://restcountries.com/v3.1/name/${nameCountry}?fields=name,capital,population,flags,languages`
   ).then(response => {
@@ -13,6 +10,3 @@ function fetchCountries(nameCountry) {
 }
 
 export { fetchCountries };
-
-// fetch(`${url}${nameCountry}${options}`).then(response => {
-//https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages
